1e4caad4f59d89846a6bf05e0a70c4094a09f399
Spencer Fuhriman <spencerfuhriman@Spencers-MacBook-Pro.local>
2017-08-27 09:42:57 -0500

    Finalize test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,42 +1,64 @@
-var request = require("browser-request");
-var cheerio = require("cheerio");
+//var request = require("browser-request");
+//var cheerio = require("cheerio");
 
+//defining elements
 var form = document.getElementById("main");
 var button = document.getElementById("submit");
 
+//setting courses
+var c100 = ["J. Williams"];
+var c101 = ["M. Buehler", "R. Gomez-Bombarelli"];
+var c200 = ["A. H. Techet", "D. Newman"];
+var c201 = ["G. Barbastathis", "A. E. Hosoi", "K. Kamrin"];
+var c300 = ["W. C. Carter", "K. Keane"];
+var c301 = ["Y. Chiang", "G. Beach", "J. Hu"];
+var c400 = ["L. Bello Gomez", "J. Jih"];
+var c401 = ["J. Jih"];
+var c500 = ["J. Deutch"];
+var c501 = ["M. Dinca", "Y. Surendranath"];
+var c600 = ["J. V. Guttag"];
+var c601 = ["R. H. Heiner"];
+var database = [
+  cl00,
+  c101,
+  c200,
+  c201,
+  c300,
+  c301,
+  c400,
+  c401,
+  c500,
+  c501,
+  c600,
+  c601
+];
+
 //Generate Button
 button.onclick = function() {
-  console.log("I SLE:FEKF");
-  var class1 = document.getElementById("class1").value;
-  var class2 = document.getElementById("class2").value;
-  var class3 = document.getElementById("class3").value;
-  var class4 = document.getElementById("class4").value;
-  var class5 = document.getElementById("class5").value;
+  var class1 = [document.getElementById("class1").value];
+  var class2 = [document.getElementById("class2").value];
+  var class3 = [document.getElementById("class3").value];
+  var class4 = [document.getElementById("class4").value];
+  var class5 = [document.getElementById("class5").value];
 
   var courses = [class1, class2, class3, class4, class5];
   var i;
+  var j;
+  var k;
   for (i = 0; i < 5; i++) {
-    console.log(courses[i]);
+    for (j = 0; j < 12; j++) {
+      if (courses[i] == database[j]) {
+        for (k = 1; k < course[i].length; k++) {}
+        courses[i][k] = database[j][k - 1];
+      }
+      break;
+    }
   }
-  //  for (i = 0; i < 5; i++){}
-  var split = courses[0].split("-");
-  var letters = split[0];
-  var pageToVisit =
-    "http://www.upenn.edu/registrar/timetable/" + letters + ".html";
-  console.log(pageToVisit);
-  request(pageToVisit, function(er, res) {
-    if (er) {
-      throw er;
-      throw response;
-      throw body;
-      console.log("I hit the error." + er);
+  for (i = 0; i < 5; i++) {
+    document.getElementById("text").innerHTML = courses[i];
+    for (j = 1; j < course[i].length; j++) {
+      document.getElementById("text").innerHTML = courses[i][j];
     }
-    console.log("Status code:" + res.statusCode);
-    console.log("Status code:" + res);
-    if (res.statusCode == "200") {
-      var $ = cheerio.load(body);
-      console.log(body);
-    }
-  });
-  console.log("past");
+    document.getElementById("text").innerHTML = "---------------------";
+  }
 };
